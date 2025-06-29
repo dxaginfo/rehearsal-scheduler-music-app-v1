25c8c09c2d57b459aaef6f206bc887f36178f754
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-06-29 07:31:46 -0300

    Initial commit front-end skeleton

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function App() {
+  return (
+    <div className="App">
+      <h1>Rehearsal Scheduler</h1>
+      <p>Web application for managing band/ensemble rehearsals.</p>
+    </div>
+  );
+}
+export default App;
